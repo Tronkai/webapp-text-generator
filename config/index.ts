50a8cfec61d273e9a866b847d8fc535ea407c5ba
Tron Kai <kerwin.zhu@tron.network>
2023-05-22 14:11:13 +0800

    change config

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import { AppInfo } from "@/types/app"
 export const APP_ID = '58623110-4ec5-4d78-a452-098445fb487c'
 export const API_KEY = 'app-gPtiOUL7FEwjgFgEzhVSZggd'
-export const API_URL = ''
+export const API_URL = 'https://api.dify.ai'
 export const APP_INFO: AppInfo = {
   "title": 'Text Generator APP',
   "description": 'App description',
